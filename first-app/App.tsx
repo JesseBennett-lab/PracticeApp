5a1a518e6865a76325cff9453c3a52fa38aba587
JesseBennett-lab <j.bennett0505@gmail.com>
2026-07-22 16:55:11 +0200

    added button

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome back</Text>
+      <Text>Enter your name:</Text>
+      <TextInput placeholder="June"/>
+      <Text>Enter your surname:</Text>
+      <TextInput placeholder="Doe"/>
+
+      <Button title= "Add user"/>
       <StatusBar style="auto" />
     </View>
   );
