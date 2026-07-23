e503e3210f3672cc2f6615e3b26619fa135d7846
JesseBennett-lab <j.bennett0505@gmail.com>
2026-07-23 16:27:37 +0200

    updated image

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,16 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View , TextInput, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View>
+
+      
+
+
      <Text style={styles.welcomeText}>Welcome to my app</Text>
-      <Text>Enter your name:</Text>
+     <Image  style={styles.logo} source={require('./images/dogimage.jpg')}/>
+
+     
+      <Text style={styles.headingText}>Enter your name</Text>
       <TextInput placeholder="Sam"/>
-      <Text>Enter your surname:</Text>
+      <Text style={styles.headingText}>Enter your surname</Text>
       <TextInput placeholder="Richards"/>
 
       <Button title= "Add user"/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,10 +26,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   welcomeText: {
-    paddingTop:50,
-    color: 'purple',
+    paddingTop:60,
+    color: 'pink',
     fontWeight: 'bold',
     fontSize: 30,
     textAlign: 'center',
+    backgroundColor:'white', 
   },
+  headingText: {
+    paddingTop:50,
+    color: 'pink',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'auto',
+    backgroundColor:'white', 
+  },
+logo:{
+  width:150,
+  height:250,
+  alignSelf: 'center',
+  
+},
+
 });
+
