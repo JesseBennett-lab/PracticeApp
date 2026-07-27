1176d33e09b4adc3940eec9a9913bcd5f2e8a0bb
JesseBennett-lab <j.bennett0505@gmail.com>
2026-07-27 11:54:39 +0200

    added screeen

--- first-app/App.tsx
+++ first-app/App.tsx
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View , TextInput, Button, Image } from 'react-native';
-
+import {useState} from 'react' ;
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
+
+ const Stack = createNativeStackNavigator();
+
   return (
-    <View>
+    <NavigationContainer>
+      <Stack.Navigator>
+  
+        <Stack.Screen name= "Home" component ={MainScreen}/>
 
-      
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
+function MainScreen(){
+
+const[Name, setName] = useState('');
+const[Surname, setSurname] = useState('');
+
+console.log("App works!");
+
+
+  return(
+  <View>
 
      <Text style={styles.welcomeText}>Welcome to my app</Text>
      <Image  style={styles.logo} 
@@ -14,15 +35,26 @@ export default function App() {
 
      
       <Text style={styles.headingText}>Enter your name</Text>
-      <TextInput placeholder="Sam"/>
+      <TextInput  style={styles.inputBoxTxt} 
+       placeholder="Sam"
+       onChangeText={newText => setName(newText)}/>
       <Text style={styles.headingText}>Enter your surname</Text>
-      <TextInput placeholder="Richards"/>
 
-      <Button title= "Add user"/>
+      <TextInput  style={styles.inputBoxTxt2}
+       placeholder="Richards"
+       onChangeText={newText => setSurname(newText)}/>
+
+      <Button title= "Add user"
+         onPress={() => {
+         console.log("Name: " + Name +
+                     "Surname: " + Surname ) 
+         }}
+      />
 
       <StatusBar style="auto" />
+    
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -41,6 +73,7 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'auto',
     backgroundColor:'white', 
+    
   },
 logo:{
   width:150,
@@ -51,6 +84,15 @@ logo:{
 inputFlex:{
   flexDirection:'row',
   marginTop:20,
+
+},
+inputBoxTxt: {
+  borderBottomWidth: 1,
+  fontSize: 20,
+},
+inputBoxTxt2: {
+  borderBottomWidth:1,
+  fontSize:20,
 }
 
 });
